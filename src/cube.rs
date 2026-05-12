e6a7538b1999d0030bdd04c07f4a08f654a6f4b5
Miguel Angel De la Vega Rodriguez <miguel.delavega@wazuh.com>
2026-05-12 15:31:03 +0200

    Fix: Rotation up and down

--- src/cube.rs
+++ src/cube.rs
@@ -19,14 +19,42 @@ impl Cube {
     }
 
     /// Eeach possible move (clockwise or anticlockwise)
-    fn up<const CW: bool>(&mut self) {
-        let tmp_11_pack = self.edges & 0x1000_0000_0000;
-        let tmp_3_pack = self.edges & 0x100;
-        self.edges = self.edges.rotate_left(4);
-        self.edges &= 0x1111_0111_1111_1111;
-        self.edges &= 0x1111_1111_1111_1011;
+    pub fn up<const CW: bool>(&mut self) {
+        let edges_face = (self.edges & 0xFFFF) as u16;
+        let rotated_edges = if CW {
+            edges_face.rotate_left(4)
+        } else {
+            edges_face.rotate_right(4)
+        };
+        self.edges = (self.edges & !0xFFFF) | (rotated_edges as u64);
+
+        let corners_face = (self.corners & 0xFFFF_FFFF) as u32;
+        let rotated_corners = if CW {
+            corners_face.rotate_left(8)
+        } else {
+            corners_face.rotate_right(8)
+        };
+        self.corners = (self.corners & !0xFFFF_FFFF) | (rotated_corners as u64);
     }
-    fn down<const CW: bool>(&mut self) {}
+
+    fn down<const CW: bool>(&mut self) {
+        let edges_face = (self.edges >> 32) as u16;
+        let rotated_edges = if CW {
+            edges_face.rotate_right(4)
+        } else {
+            edges_face.rotate_left(4)
+        };
+        self.edges = (self.edges & !0xFFFF_0000_0000) | ((rotated_edges as u64) << 32);
+
+        let corners_face = (self.corners >> 32) as u32;
+        let rotated_corners = if CW {
+            corners_face.rotate_right(8)
+        } else {
+            corners_face.rotate_left(8)
+        };
+        self.corners = (self.corners & !0xFFFF_FFFF_0000_0000) | ((rotated_corners as u64) << 32);
+    }
+
     fn right<const CW: bool>(&mut self) {}
     fn left<const CW: bool>(&mut self) {}
     fn top<const CW: bool>(&mut self) {}
